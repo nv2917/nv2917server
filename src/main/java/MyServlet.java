f59f60a0fc01e3455c609436152e7d12842fdb77
nv2917 <nv2917@ic.ac.uk>
2020-11-19 18:00:58 +0000

    2

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -11,7 +11,7 @@ public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        resp.getWriter().write("Hello, world!" + req.getServletPath() );
+        resp.getWriter().write("Hello, world!" + "<b>Hello Christos</b>" );
 
     }
 
